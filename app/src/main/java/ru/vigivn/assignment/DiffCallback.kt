048966bc087c4c2f4d7f3128ec421d3f9d92282f
Vigiv <42895139+Vigiv@users.noreply.github.com>
2021-01-03 21:33:30 +0300

    Remove diff

--- app/src/main/java/ru/vigivn/assignment/DiffCallback.kt
+++
@@ -1,9 +0,0 @@
-package ru.vigivn.assignment
-
-import androidx.recyclerview.widget.DiffUtil
-
-class DiffCallback: DiffUtil.ItemCallback<String>() {
-    override fun areItemsTheSame(oldItem: String, newItem: String): Boolean = oldItem == newItem
-
-    override fun areContentsTheSame(oldItem: String, newItem: String): Boolean = oldItem == newItem
-}
